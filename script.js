0224021a35f6a85ae76dfbc1fab36743076c1b18
dune123 <96974209+dune123@users.noreply.github.com>
2024-10-29 14:25:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	const minHeap=new minHeap(arr);
+	let total=0;
+	while(minHeap.size()>1){
+		let cost1=minHeap.extractMin();
+		let cost2=minHeap.extractMin();
+
+		let cost=cost1+cost2;
+
+		totalCost += cost;
+		minHeap.insert(cost);
+	}
+	return totalcost;
 }
 
 module.exports=mincost;
